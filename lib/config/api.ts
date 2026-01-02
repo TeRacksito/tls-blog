b39a2b9c83867600422bdd4a930205d089599ae2
TeRacksito <acho.chuchi.8@gmail.com>
2026-01-02 18:27:27 +0000

    Fix constant naming for API base URLs in configuration

--- lib/config/api.ts
+++ lib/config/api.ts
@@ -1,14 +1,14 @@
-const defaultNodeApiBaseUrl = 'https://new.tls-unbound.es/api';
-const defaultNextApiBaseUrl = '/nextapi';
+const DEFAULT_NODE_API_BASE_URL = 'https://new.tls-unbound.es/api';
+const DEFAULT_NEXT_API_BASE_URL = '/nextapi';
 
 /**
  * Node.js API base URL.
  */
 export const NODE_API_BASE_URL =
-  process.env.NODE_API_BASE_URL ?? defaultNodeApiBaseUrl;
+  process.env.NODE_API_BASE_URL ?? DEFAULT_NODE_API_BASE_URL;
 
 /**
  * Next.js internal API base URL.
  */
 export const NEXT_API_BASE_URL =
-  process.env.NEXT_API_BASE_URL ?? defaultNextApiBaseUrl;
+  process.env.NEXT_API_BASE_URL ?? DEFAULT_NEXT_API_BASE_URL;
