b7d691ccfc31157cf86cc89b4e36e370b3330ccc
TeRacksito <acho.chuchi.8@gmail.com>
2026-03-22 18:18:21 +0000

    More Minecraft like style

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,29 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { AuthProvider } from '@/components/auth/AuthProvider';
 import { LoginModal } from '@/components/auth/LoginModal';
-import { Geist } from 'next/font/google';
+import {
+  JetBrains_Mono,
+  Nunito,
+  Press_Start_2P,
+  PT_Serif,
+} from 'next/font/google';
 import { cn } from '@/lib/utils';
 
-const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+const nunito = Nunito({ subsets: ['latin'], variable: '--font-sans' });
+const ptSerif = PT_Serif({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400', '700'],
+});
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
+const pressStart2p = Press_Start_2P({
+  subsets: ['latin'],
+  variable: '--font-pixel',
+  weight: '400',
+});
 
 export const metadata: Metadata = {
   title: 'TLS Web',
@@ -18,7 +37,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className={cn('font-sans', geist.variable)}>
+    <html
+      lang="es"
+      className={cn(
+        'font-sans',
+        nunito.variable,
+        ptSerif.variable,
+        jetBrainsMono.variable,
+        pressStart2p.variable
+      )}
+    >
       <body className="antialiased">
         <AuthProvider>
           {children}
