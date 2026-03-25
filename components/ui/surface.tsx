cd5bfde4d6601e528e4ef750615061cb9e48746b
TeRacksito <acho.chuchi.8@gmail.com>
2026-03-25 15:59:39 +0000

    Add surface primitive.
    It enables shadows for child component

---
+++ components/ui/surface.tsx
@@ -0,0 +1,38 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Slot } from 'radix-ui';
+
+const surfaceVariants = cva('', {
+  variants: {
+    hasShadow: {
+      true: 'shadow-md',
+      false: 'shadow-none',
+    },
+    hasTextShadow: {
+      true: 'text-shadow-[var(--shadow-md-base)_var(--shadow-md-color)]',
+      false: 'text-shadow-none',
+    },
+  },
+  defaultVariants: {
+    hasShadow: false,
+    hasTextShadow: false,
+  },
+});
+
+function Surface({
+  className,
+  hasShadow,
+  hasTextShadow,
+  asChild,
+  ...props
+}: React.HTMLAttributes<HTMLElement> &
+  VariantProps<typeof surfaceVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot.Root : 'div';
+  return (
+    <Comp
+      className={surfaceVariants({ hasShadow, hasTextShadow, className })}
+      {...props}
+    />
+  );
+}
+
+export { Surface, surfaceVariants };
